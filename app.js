0cbcdae0f04bced537e84c23e81714b8557a155a
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-04-30 15:54:41 +0545

    explored initial routing

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 
 let express = require("express");
 const app = express();
+
+app.get("/", (req, res)=>{  //(in sequence request , response)
+    res.send("Hello world!");
+})
+
+app.get("/api/user", function(req, res){
+    res.send("This is route to user");
+})
+
+
 app.listen(5000, function(){
-    console.log("Server has started at port: 5000");
+    console.log("server has started at port: 5000");
 })
